fbce06b55a1c87d92aed7518bacf277d36657cdc
Nicolas Geoffray <ngeoffray@google.com>
2019-02-22 15:38:22 +0000

    raphael: Generate the apex image.
    Change-Id: I18a76766590db27a4c32534dc948f55f78da166c
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,9 @@ DEVICE_PATH := device/xiaomi/raphael
 # APEX
 OVERRIDE_TARGET_FLATTEN_APEX := true
 
+# APEX image
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
