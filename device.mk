0cfe8b60b5de8626152f30404718d0e4c3677b4a
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 02:09:34 +0530

    raphael: rro_overlays: Move CarrierConfig to RRO

--- device.mk
+++ device.mk
@@ -407,6 +407,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlay \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
