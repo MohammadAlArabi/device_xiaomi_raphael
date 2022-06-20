678bba95777a04a516145f6bbad460a4711301da
Pranav Temkar <pranavtemkar@gmail.com>
2022-06-20 06:48:43 +0000

    Revert "raphael: overlay: Adjust biometric dialog for 11"
    Adapt for A12.1 / L
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    This reverts commit 01eaa3abe178f3d20e6904c452383da6f72d93a5.

--- device.mk
+++ device.mk
@@ -45,8 +45,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay-evolution
 
 # ANT+
 PRODUCT_PACKAGES += \
