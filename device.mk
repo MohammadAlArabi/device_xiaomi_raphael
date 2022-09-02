6bbca0c3b62b7343927c26dba57958584f3fe77d
chandu078 <chandudyavanapelli03@gmail.com>
2022-09-02 23:39:54 +0200

    raphael: libshim for vendor/lib64/libcdfw_remote_api.so
     This fix the missing symbol: F linker  : CANNOT LINK EXECUTABLE
     "xtra-daemon": cannot locate symbol "_ZN8loc_util8LocTimerD2Ev" referenced by
     "/vendor/lib64/libcdfw_remote_api.so"...
    
    Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>

--- device.mk
+++ device.mk
@@ -443,6 +443,7 @@ PRODUCT_COPY_FILES += \
 
 # Shim
 PRODUCT_PACKAGES += \
+    libshim_libcdfw_remote_api \
     libwatermark_shim
 
 # Telephony
---
+++ libshim/libshim_libcdfw_remote_api.cpp
@@ -0,0 +1,4 @@
+extern "C" {
+
+   void _ZN8loc_util8LocTimerD2Ev() {}
+}
