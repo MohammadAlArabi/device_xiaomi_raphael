20f085a0f9e12042cd0c93edee329a41c2e75228
Mikhail Naganov <mnaganov@google.com>
2022-09-22 20:15:58 +0000

    Uprev audio hal to 7.0
    Updated audio policy manager configuration files to comply
    with the changes in the XML schema.
    
    Bug: 142480271
    Test: atest VtsHalAudioV7_0TargetTest
    Test: atest VtsHalAudioEffectV7_0TargetTest
    Change-Id: I8e4b132e42b1f6603a3382fe970109ef701220b8
    Signed-off-by: pc1598 <piyushchouhan1598@gmail.com>
    Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>

--- device.mk
+++ device.mk
@@ -55,8 +55,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
