6c5a55d473557aff5dd458ea50e12196be3740ec
Jakub Pawlowski <jpawlowski@google.com>
2022-09-23 10:47:34 +0700

    raphael: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Signed-off-by: pc1598 <piyushchouhan1598@gmail.com>

--- device.mk
+++ device.mk
@@ -96,6 +96,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     android.hardware.bluetooth.audio-impl \
     libldacBT_dec \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
