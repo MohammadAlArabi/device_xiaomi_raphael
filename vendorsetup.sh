c15121d27a1e2a0639529c9355158f4db39f855f
pc1598 <piyushchouhan1598@gmail.com>
2022-09-27 02:59:41 +0000

    Switch to Aosp Clang
    Signed-off-by: SageOfD6Path <mai2anirban95@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,4 +2,3 @@ rm -rf hardware/qcom-caf/sm8150/display
 git clone https://github.com/PptO07/hardware_qcom_display.git -b 12-sm8150 hardware/qcom-caf/sm8150/display --depth=1
 git clone https://github.com/VoltageOS-Devices/vendor_xiaomi_raphael.git -b 13 vendor/xiaomi/raphael --depth=1
 git clone https://github.com/VoltageOS-Devices/kernel_xiaomi_raphael.git -b 13 kernel/xiaomi/raphael --depth=1
-git clone https://gitlab.com/varunhardgamer/trb_clang.git -b 16 prebuilts/clang/host/linux-x86/trb_clang --depth=1
