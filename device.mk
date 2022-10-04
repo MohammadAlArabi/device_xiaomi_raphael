fb1a66ae1364fffcffa8a7ca60e20116fffd9eed
onettboots <blackcocopet@gmail.com>
2022-10-04 13:05:48 +0000

    raphael: Build OtoMusicplayer
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- device.mk
+++ device.mk
@@ -35,6 +35,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# OtoMusicPlayer
+PRODUCT_PACKAGES += \
+    OtoMusicPlayer
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
