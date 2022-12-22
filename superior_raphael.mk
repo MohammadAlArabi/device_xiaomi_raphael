d944d0906ae75c0bdf2d4dce3d6376c27ec2d5ae
SageOfD6Path <mail2anirban95@gmail.com>
2022-12-22 08:32:28 +0000

    Enable Udfps Resources

--- superior_raphael.mk
+++ superior_raphael.mk
@@ -33,6 +33,8 @@ TARGET_INCLUDE_PIXEL_CHARGER := true
 # Disable/Enable Blur Support, default is false
 TARGET_ENABLE_BLUR := true
 
+#Udfps Stuff
+SUPERIOR_UDFPS_ANIMATIONS := true
 # Quick Tap Feature
 TARGET_SUPPORTS_QUICK_TAP := true
 
