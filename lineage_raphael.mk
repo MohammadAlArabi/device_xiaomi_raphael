95494a55ef2e80b4f83ca38c1d9366b7bf7a019c
onettboots <blackcocopet@gmail.com>
2022-12-25 17:46:35 +0000

    raphael: build faceunlok,quick tap, and extra udsfps animations"
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -27,6 +27,11 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+# UDFPS animations
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_raphael
 PRODUCT_DEVICE := raphael
