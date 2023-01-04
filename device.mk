97e0fcc3e842b942a80968735a41d4b321e4c79a
firebird11 <hbgassel@gmail.com>
2023-01-04 19:28:22 +0100

    raphael: Enable fingerprintAnimation
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -194,6 +194,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Fingerprint animation
+TARGET_HAS_UDFPS := true
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
