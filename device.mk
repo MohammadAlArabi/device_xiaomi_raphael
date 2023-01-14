21edcb8628c96db2e9088d152c3dcb4f3517afda
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-01-14 13:29:04 +0800

    raphael: Build sensors.raphael_udfps
    Referenced:
    https://github.com/yaap/device_oneplus_sm8150-common/commit/22f85f64ec7578bcb07c409a53eb8294c69e3d67
    Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>

--- device.mk
+++ device.mk
@@ -430,7 +430,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge \
-    sensors.udfps
+    sensors.raphael_udfps
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
