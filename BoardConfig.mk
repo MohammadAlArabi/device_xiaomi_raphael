ab75fd5b44a2cfd1f36b998b6edcbd0a06b409f9
InVictusXV <aradheakiki123@gmail.com>
2023-05-12 21:40:23 +0700

    Revert "raphael: Add Livedisplay 2.1"
    This reverts commit da9028e9ce31e71d06c7be3607be58b26c590a6b.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,8 +79,6 @@ TARGET_USES_FOD_ZPOS := true
 BOARD_HAVE_QCOM_FM := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    vendor/evolution/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/hidl/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/hidl/framework_compatibility_matrix.xml
