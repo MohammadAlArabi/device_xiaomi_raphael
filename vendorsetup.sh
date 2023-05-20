db125e9d04a2c97f08e057d69214bd795c91e512
MohammadAlArabi <mohammadkingoftheworld@gmail.com>
2023-05-20 18:10:30 +0000

    raphael: Drop vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,7 +0,0 @@
-
-rm -rf hardware/qcom-caf/sm8150/display
-git clone https://github.com/SageOfD6Path/hardware_qcom_display.git -b 13-sm8150 hardware/qcom-caf/sm8150/display --depth=1
-git clone https://github.com/VoltageOS-Devices/vendor_xiaomi_raphael.git -b 13 vendor/xiaomi/raphael --depth=1
-git clone https://github.com/VoltageOS-Devices/kernel_xiaomi_raphael.git -b 13 kernel/xiaomi/raphael --depth=1
-git clone https://gitlab.com/XSans0/weebx-clang.git -b release/15-gr prebuilts/clang/host/linux-x86/clang-weebx --depth=1
-
