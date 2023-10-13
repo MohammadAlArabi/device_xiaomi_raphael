b8b209d76ffa7b038fd9aded4e5de2d163537c2c
MohammadAlArabi <mohammadkingoftheworld@gmail.com>
2023-10-13 20:43:11 +0000

    raphael: Swith to Selinux permissive [temp]

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,7 +123,7 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += loop.max_part=16
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += cgroup_disable=pressure
